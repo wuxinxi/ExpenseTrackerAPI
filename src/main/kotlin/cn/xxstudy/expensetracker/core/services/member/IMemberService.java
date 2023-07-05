1f0a23eaf58137af5d15ad0c88d3b31741c6f5d5
sensi <sensi.wu@tecpal.com>
2023-07-05 17:38:13 +0800

    add member api

---
+++ src/main/kotlin/cn/xxstudy/expensetracker/core/services/member/IMemberService.java
@@ -0,0 +1,6 @@
+package cn.xxstudy.expensetracker.core.services.member;/**
+ * @date: 2023/7/5 09:17
+ * @author: TangRen
+ * @remark:
+ */public interface IMemberService {
+}
---
+++ src/main/kotlin/cn/xxstudy/expensetracker/core/services/member/MemberService.java
@@ -0,0 +1,6 @@
+package cn.xxstudy.expensetracker.core.services.member;/**
+ * @date: 2023/7/5 09:18
+ * @author: TangRen
+ * @remark:
+ */public class MemberService {
+}
